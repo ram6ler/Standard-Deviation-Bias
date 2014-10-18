652d9f8277b877917434ae3ced1c2b1b4816375f
Richard Ambler <rambler@ibwya.net>
2014-10-18 21:54:30 +0800

    improved comments; removed redundant code

--- sd_dists.R
+++ sd_dists.R
@@ -1,17 +1,19 @@
-# Demonstrate how sd calculated with denominator n
-# introduces bias compared to sd calculated 
+# Demonstration of sample sd calculated with denominator n being
+# biased (compared to sd calculated with denominator n - 1) as an
+# estimate of the population sd.
 
 library("trotter")
 
 pop.size <- 20
 samp.size <- 5
 
+# Generate a random population
 my.population <- rnorm(pop.size)
+# Get the actual parameter
 pop.sd <- sd(my.population) * (pop.size - 1) / pop.size
 
-indices <- 1:pop.size
-
 # Generate a pseudo-vector containing all sample combinations
+indices <- 1:pop.size
 all.samples <- cpv(samp.size, indices)
 
 # Sample standard deviations calculated with denominator n
@@ -34,26 +36,25 @@ sd.n_1 <- sapply(
 
 par(mfrow = c(2, 1))
 
+show.pop.sd <- function() {
+  abline(
+    v = pop.sd,
+    lty = "dashed",
+    lwd = "2",
+    col = "red"
+  )
+}
+
 hist(
   sd.n,
   main = "Distribution of sample SD; denominator n",
   xlab = "Standard deviation"
 )
-abline(
-  v = pop.sd,
-  lty = "dashed",
-  lwd = "2",
-  col = "red"
-)
+show.pop.sd()
 
 hist(
   sd.n_1,
   main = "Distribution of sample SD; denominator n - 1",
   xlab = "Standard deviation"
 )
-abline(
-  v = pop.sd,
-  lty = "dashed",
-  lwd = "2",
-  col = "red"
-)
+show.pop.sd()
